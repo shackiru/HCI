24cc181819c6b90a69ae7c4b8293b0fbe46a14fe
shaqqputra <84982248+shaqqputra@users.noreply.github.com>
2023-03-18 22:15:15 +0700

    jenshin impekz

--- ExpandingCard/script.js
+++ ExpandingCard/script.js
@@ -1,17 +1,30 @@
 const panels = document.querySelectorAll(".content-panel")
-panels.forEach(panel => 
-    {
-        panel.addEventListener("mouseenter", ()=>
-        {
+const content = document.querySelectorAll(".content")
+
+panels.forEach((panel, index) => {
+        panel.addEventListener("mouseenter", () => {
             removeActiveClass()
             panel.classList.add("active")
+            content[index].style.display = 'flex'
+            console.log(content.length)
         })
     })
 
-function removeActiveClass()
-{
-    panels.forEach(panel =>
-        {
-            panel.classList.remove("active")
+panels.forEach((panel) => {
+    panel.addEventListener("mouseleave", () => {
+        removeActiveClass()
+    })
+})
+    
+    function removeActiveClass()
+    {
+        panels.forEach(panel =>
+            {
+                for(let i = 0; i < content.length; i++)
+                {
+                    content[i].style.display = 'none'
+                }
+                panel.classList.remove("active")
+
         })
 }
